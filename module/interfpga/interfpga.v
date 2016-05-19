c5ac9c95aec3d8e1f38497b0ea4eb0c9df183141
BobbyL2k <bobby_nh@hotmail.com>
2016-05-19 16:14:32 +0700

    add comments to interfpga

--- module/interfpga/interfpga.v
+++ module/interfpga/interfpga.v
@@ -1,21 +1,36 @@
+// module for sending signal from and FPGA to another FPGA
 module interfpga_send(
-  input wire [7:0] data,
-  input wire send,
-  output wire busy,
-  output reg [3:0] data_o,
-  output wire ctrl_o,
-  input wire reset,
-  input wire clk
+  input wire [7:0] data,    // Input data
+  input wire send,          // Signal the module to send the data 
+                            // (data is ready) - active high
+  output wire busy,         // Indicating that the module is busy
+                            // sending data - active high
+  output reg [3:0] data_o,  // lines used to send data
+  output wire ctrl_o,       // line used to send control signal
+  input wire reset,         // reset - active high
+  input wire clk            // clock, this module uses positive edge
+                            // to update it's FF
 );
 
-parameter sWait = 3'b000;
-parameter sSend = 3'b100;
+// state of this module contains the following
+parameter sWait = 3'b000;   // indicates that the module is waiting to
+                            // send data
+parameter sSend = 3'b100;   // indicates that the module is sending data
+                            // has 4 sub states (to send 8 bits, 4x2)
+                            // 100, 101, 110, 111
+                            // the same data is held for 2 clocks
+                            // ex. 100 and 101 has the same data
 
-reg [2:0] c_state, n_state;
+reg [2:0] c_state, n_state; // module state
+                            // c_state - current state
+                            // n_state - next state
 
-assign busy = c_state[2];
-assign ctrl_o = c_state[2];
+assign busy = c_state[2];   // the module sends a busy signal while in
+                            // 1XX state 
+assign ctrl_o = c_state[2]; // while sending the control line is high
 
+// current state (c_state) block
+// assign the next state (n_state) to the current state
 always @( posedge clk or reset ) begin
   if(reset) begin
     c_state = sWait;
@@ -24,6 +39,11 @@ always @( posedge clk or reset ) begin
   end 
 end
 
+// next state (n_state) block
+// calculate the next state (sequential logic)
+// if the current state is in sSend the and until overflow to sWait
+// if recived send signal then go to sSend
+// else stay at sWait
 always @( * ) begin
   if( c_state[2] == 1 ) begin
     n_state = c_state + 1;
@@ -34,47 +54,72 @@ always @( * ) begin
   end 
 end
 
+// send the 4 least singificant bit first (c_state = 10X)
+// then send the 4 most singificant bit first (c_state = 11X)
 always @( * ) begin
-  if( c_state[1] == 1 ) begin
-    data_o = data[7:4];
-  end else begin
+  if( c_state[1] == 0 ) begin
     data_o = data[3:0];
+  end else begin
+    data_o = data[7:4];
   end 
 end
 
 endmodule // interfpga_send
 
+
+// module for reciving signal from another FPGA
 module interfpga_receive(
-  output wire [7:0] data,
-  output wire ready,
-  input wire reset_ready,
-  input wire [3:0] data_i,
-  input wire ctrl_i,
-  input wire reset,
-  input wire clk//,
-  //output wire [15:0] bufx
+  output wire [7:0] data,   // Outputs data recived 
+  output wire ready,        // Indicate that the data recived is 
+                            // ready to be read - active high
+  input wire reset_ready,   // resets the ready signal - active high
+  input wire [3:0] data_i,  // lines used to recive data 
+  input wire ctrl_i,        // line used to recive control signal
+  input wire reset,         // reset - active high
+  input wire clk            // clock, this module uses positive edge
+                            // to update it's FF
 );
 
-reg [7:0] buffer [1:0];
+reg [7:0] buffer [1:0];     // buffer for stroing data recived
+                            // this module uses double buffering
 reg c_buffer_select, n_buffer_select;
+                            // buffer selector state
+                            // c_buffer_select - current state
+                            // n_buffer_select - next state
 assign data = buffer[c_buffer_select];
-//assign bufx[15:8] = buffer[1];
-//assign bufx[7:0] = buffer[0];
+                            //  data outputed is from
+                            // the selected buffer
 
+// state of this module contains the following
 parameter sWait      = 3'b000;
+                            // indicates that the module is waiting to
+                            // recive data
 parameter sReceiving = 3'b100;
+                            // indicates that the module is reciving data
+                            // has 4 sub states (to recive 8 bits, 4x2)
+                            // 100, 101, 110, 111
+                            // the same data is held for 2 clocks
+                            // ex. 100 and 101 has the same data
 
-reg [2:0] c_state, n_state;
-reg p_state_2;
+reg [2:0] c_state, n_state; // module state
+                            // c_state - current state
+                            // n_state - next state 
+reg p_state_2;              // p_state_2 stores c_state[2] in
+                            // in the last clock
 
-parameter sNotReady  = 1'b0;
-parameter sReady     = 1'b1;
+// states of data outputed contains the following
+parameter sNotReady  = 1'b0;// data is ready to be read 
+parameter sReady     = 1'b1;// data is not ready to be read
 
-reg c_ready, n_ready;
+reg c_ready, n_ready;       // data ready state
+                            // c_ready - current state
+                            // n_ready - next state
 
-assign ready = c_ready;
-
-// Module main state
+assign ready = c_ready;     // send ready signal when in
+                            // ready state
+                            
+// current state (c_state) block
+// assign the next state (n_state) to the current state
 always @( posedge clk or reset ) begin
   if(reset) begin
     c_state = sWait;
@@ -84,6 +129,7 @@ always @( posedge clk or reset ) begin
 end
 
 // Module prev-main state
+// assign the last c_state[2] to p_state_2
 always @( posedge clk or reset ) begin
   if(reset) begin
     p_state_2 = sWait;
@@ -92,6 +138,11 @@ always @( posedge clk or reset ) begin
   end 
 end
 
+// next state (n_state) block
+// calculate the next state (sequential logic)
+// if the current state is in sReceiving the and until overflow to sWait
+// if recived send signal then go to sReceiving
+// else stay at sWait
 always @( * ) begin
   if( c_state[2] == 1 ) begin
     n_state = c_state + 1;
